39b6f1a0a69ece27bb30f98e2a8f04fb1749411f
renataap13 <renataap_1810@hotmail.com>
2023-07-03 16:28:15 -0300

    formu
    programa

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-Document.getElementBlyId('enviar').addEventListener('click',function(event){
+Document.getElementBlyId('formu').addEventListener('submit',function(event){
     event.preventDefault()
 
-function event(){
     let  nome = document.getElementById("nome").value ;
     let  email = document.getElementById("email").value ;
     
     console.log('Nome:',nome);
     console.log('Email:',email);    
-}
+
+let FormData = new FormData();
+FormData.append("Email", email);
+
+fetch("html://formspree.io/f/xzblplze", {
+  method: "post",
+  headers: {
+    "Accept": "application/json"
+  },
+  body: FormData 
+
+})
+
+.then(function(response) {
+  // Trata a resposta da da requisiçâo
+  if(response.ok) {
+   alert("Formilario enviado com sucesso!");
+  } else {
+    alert("Ocorreu um erro ao enviar o formulario. Por favor, tente novamente tarde")
+  }
+})
+
+.catch(function(error) {
+  alert("ocrreu um erro ao enviar o formulario. Por favor,tente novamente mais tarde")
+});  
 });
