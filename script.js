7e7cc1622ae7a5f2d0754cfb5be233df6ff4aceb
renataap13 <renataap_1810@hotmail.com>
2023-07-03 15:10:22 -0300

    email
    nome

---
+++ script.js
@@ -0,0 +1,11 @@
+Document.getElementBlyId('enviar').addEventListener('click',function(event){
+    event.preventDefault()
+
+function event(){
+    let  nome = document.getElementById("nome").value ;
+    let  email = document.getElementById("email").value ;
+    
+    console.log('Nome:',nome);
+    console.log('Email:',email);    
+}
+});
